34d94ead5d1bf17c07940534e328b60ad3e5db01
Vinderruck <majevincent0@gmail.com>
2024-09-10 11:55:27 +0300

    search button and search input for sm screens

--- sale/src/Components/Navbar.jsx
+++ sale/src/Components/Navbar.jsx
@@ -12,6 +12,7 @@ import { Appcontext } from '../Global/Global';
 const Navbar = () => {
   const {mood,handlethemechange} =useContext(Appcontext)
   const [Sign, setSign] = useState(false)
+  const [search, setsearch] = useState(false)
 
    
  
@@ -27,7 +28,15 @@ const Navbar = () => {
    <div className="flex justify-center items-center bg-slate-200 hover:bg-white shadow-md ml-6 rounded w-[30px] h-[30px] r-0"> 
     <CiMenuKebab  className="p-1 w-full h-full"/></div>
     <div  className="flex justify-center items-center sm:hidden bg-slate-200 hover:bg-white shadow-md mr-6 rounded w-[30px] h-[30px]">
-       <IoMdSearch/></div>
+       <IoMdSearch onClick={()=>setsearch(!search)}/>
+        {search &&(
+          <div className="z-4 flex justify-center items-center bg-blue-200 bg-opacity-90 shadow-black shadow-md backdrop-blur-md border rounded-md w-full h-[auto] searchsm">
+          <input type="text" placeholder="enter your search " className="items-center border-x-slate-100 bg-slate-100 hover:bg-white shadow-md m-1 border rounded w-1/2 text-[16px] text-black text-center paragraph"/>
+        &nbsp; <button onClick={()=>setsearch(false)} className="items-center border-slate-300 bg-slate-200 hover:bg-white shadow-md p-1 border rounded w-3/12 text-[16px] text-center paragraph">search</button>
+      
+       </div>
+        )}
+       </div>
    <div className="sm:flex flex-1 justify-center items-center hidden m-4 w-1/2">
     <input type="text" placeholder="enter your search " className="items-center border-x-slate-100 bg-slate-100 hover:bg-white shadow-md m-1 border rounded w-1/2 text-[16px] text-black text-center paragraph"/>
   &nbsp; <button className="items-center border-slate-300 bg-slate-200 hover:bg-white shadow-md p-1 border rounded w-3/10 text-[16px] text-center paragraph">search</button>
