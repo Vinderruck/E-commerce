73971e9d017a9172909080e415426fe1c27e18b8
Vinderruck <majevincent0@gmail.com>
2024-09-12 07:23:30 +0300

    shopping

--- sale/src/Components/Shopping.jsx
+++ sale/src/Components/Shopping.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Shopping = () => {
   return (
-    <div className="Product">Shopping</div>
+    <div className="flex justify-center items-center Product">
+        <div className="justify-center items-center bg-slate-200 m-9 w-1/2 h-1/z"> hello</div>
+        </div>
   )
 }
 
--- sale/src/Components/index.js
+++ sale/src/Components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Products from "./Products";
 import Shopping from "./Shopping";
+import Recomandation from './Recomandation'
 
-export {Navbar,Hero,Products,Shopping};
+export {Navbar,Hero,Products,Shopping,Recomandation};
