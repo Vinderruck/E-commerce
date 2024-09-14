4c5c71834d2db4103c612dba77ab3168d699a7ff
Vinderruck <majevincent0@gmail.com>
2024-09-15 00:43:15 +0300

    change

--- sale/src/Components/Admin/Login.jsx
+++ sale/src/Components/Admin/Login.jsx
@@ -3,7 +3,7 @@
  import axios from 'axios';
  
  const   login = () => {
-  document.documentElement.style.background = "black";
+  document.documentElement.style.background = "white";
 
  const [Email, setEmail] = useState("")
  const [Name, setName] = useState("")
@@ -32,17 +32,17 @@
    return (
   
  
-      <div className="w-full h-[100vh]">
+      <div className="w-full h-[80vh]">
        
-        <div className="flex justify-center items-center bg-slate-200 bg-opacity-70 mt-10 p-3 rounded-xl w-full h-full login">
+        <div className="flex justify-center items-center bg-slate-200 bg-opacity-70 mt-12 p-1 rounded-xl w-full h-full login">
     
       <Form  onSubmit={handlesubmit}  className="flex flex-col justify-center items-center bg-blue-200 bg-opacity-40 p-1 rounded-lg w-full">
       <div className="bg-slate-400 bg-opacity-40 m-4 border rounded-md w-full text-center"><h1 className="paragraph">ADMIN LOGIN</h1></div>
 <Form.Group className="flex flex-col justify-center items-center m-3">
 <Form.Label className="m-2 paragraph">Your Email :</Form.Label>
-    <Form.Control placeholder="Enter your Name" value={Email} onChange={(e)=>setEmail(e.value)} type="text" className="hover:bg-slate-300 border text-center paragraph rounnded-lg"/>
+    <Form.Control placeholder="Enter your Name" value={Email} onChange={(e)=>setEmail(e.value)} type="text" className="hover:bg-slate-300 border w-full text-center paragraph rounnded-lg"/>
     </Form.Group>
-    <Form.Group  className="flex flex-col justify-center items-center m-3">
+    <Form.Group  className="flex flex-col justify-center items-center m-3 w-full">
 <Form.Label className="m-2 paragraph">Your Name : </Form.Label>
     <Form.Control placeholder="Enter your Name" type="text" value={Name} onChange={(e)=>setName(e.value)}  className="paragraph hover:rounnded-lg border-white hover:bg-slate-300 border text-center"/>
     </Form.Group>
