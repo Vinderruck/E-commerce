a926d58b692f1357837c7f0771b4b1a3fc8551bb
Vinderruck <majevincent0@gmail.com>
2024-09-21 11:59:45 +0300

    hero section

--- sale/src/Components/Hero/Hero.jsx
+++ sale/src/Components/Hero/Hero.jsx
@@ -27,36 +27,49 @@ import  {women1,women2,women3,women4,women5,shoes1,shoes2,
 } from '../../assets';
  
 const Hero = () => {
-
+ 
  const imagesale = [
-  men1,men2,women1,Acce1
+  men1,men2,women1,women2,shoes1,child1
+ ]
+ const image = [
+  Elec1,Acce2,phone1
+ ]
+ const funiture = [
+  funi1,funi2
  ]
  const [currentIndex, setCurrentIndex] = useState(0);
-
+ const [currentIndex1, setCurrentIndex1] = useState(0);
+ const [currentIndex2, setCurrentIndex2] = useState(0);
 useEffect(() => {
   const intervalId = setInterval(() => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % imagesale.length);   
-
+    setCurrentIndex1((prevIndex) => (prevIndex + 1) % image.length);   
+    setCurrentIndex2((prevIndex) => (prevIndex + 1) % funiture.length);   
   },10000);
 
  return () => clearInterval(intervalId);
 }, []);
 
+const currentImage1 =image[currentIndex1]
+const currentImage2= funiture[currentIndex2]
 const currentImage = imagesale[currentIndex];
   return (
-    <div className="justify-between shadow-black shadow-lg mt-12 p-2 rounded-lg w-full h-auto Hero">
+    <div className="flex justify-center items-center shadow-black shadow-lg mt-12 p-2 rounded-lg w-full h-1/2 Hero">
 
-<div className="border-slate-300 shadow-md p-2 border rounded-xl w-full h-auto">
-  <div className="m-2"><h1 className="font-bold text-[16 text-center text-orange-500 heading px]">All your Shopping is right here</h1></div>
-  <div className="flex justify-center items-center w-11/12">
-  <img src={currentImage} alt="Product image" className="shadow-md rounded-xl w-1/2 h-[300px] sm:h-[400px] object-cover" />
-  </div>
-
-  
-    </div>
-     
+<div className="border-slate-300 shadow-md border rounded-xl w-full h-full center">
+      <div className="flex justify-center items-center m-2">
+        <h1 className="font-bold text-[16px] text-center text-orange-500 heading">All your Shopping is right here</h1></div>
+      <div className="flex sm:flex-row flex-col justify-between items-center gap-1 m-3 p-1 w-1/2">
+      <img src={currentImage} alt="Product image" className="shadow-md rounded-xl w-full h-[200px] image" />
+      <img src={currentImage1} alt="Product image" className="shadow-md rounded-xl w-full h-[200px] image" />
+      <img src={currentImage2} alt="Product image" className="shadow-md rounded-xl w-full h-[200px] image" />
       
     </div>
+      
+        </div>
+         
+          
+        </div>
   )
 }
 
