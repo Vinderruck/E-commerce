7fc4a08422c7a5900df679db244bb43dcf77e661
Vinderruck <majevincent0@gmail.com>
2024-09-21 19:58:32 +0300

    Admin panel

--- sale/src/Components/Admin/Navbar/Nav.jsx
+++ sale/src/Components/Admin/Navbar/Nav.jsx
@@ -3,6 +3,9 @@ import { logo } from '../../../assets';
 import { CiMenuKebab } from "react-icons/ci";
 import { MdLightMode} from "react-icons/md";
 import { Appcontext } from '../../../Global/Global';
+import './Navbar.css'
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
  
  
  const Nav = () => {
@@ -13,28 +16,36 @@ useEffect(() => {
 
 //statechanger
 const [Links, setLinks] = useState(false)
-
+const [active, setActive] = useState(false)
    return (
      <div className="flex flex-col justify-between items-start">
-        <div className="z-10 fixed flex justify-between items-start bg-slate-200 opacity-80 backdrop-blur-md w-full">
+        <div className="z-10 fixed flex justify-between items-center bg-slate-200 opacity-80 backdrop-blur-md w-full">
+        <div className="flex justify-center items-center bg-slate-200 hover:bg-white shadow-md ml-1 rounded w-[35px] h-[35px] r-0">
+<CiMenuKebab onClick={()=> setLinks(!Links)} className="p-1 w-full h-full"/>
+{Links && (
+  <div className="top-0 right-0 left-0 fixed flex flex-col justify-center items-center shadow-md shadow-white mt-24 rounded-md w-1/2 sm:w-1/5 overflow-auto slide">
+<p className="active:bg-white p-2 paragraph" ><a href="" ClassName={active ? "active" : ""}  onClick={()=>setActive(!active)}>Product Management</a></p>
+<p className="p-2 paragraph"><a href="">Order Management</a></p>
+<p className="p-2 paragraph"><a href="">Payment and Shipping</a></p>
+<p className="p-2 paragraph"><a href="">Reporting and Analytics</a></p>
+<p className="p-2 paragraph"><a href="">Marketing and Promotion</a></p>
+<p className="p-2 paragraph"><a href="">Content Management</a></p>
+<p className="p-2 paragraph"><a href="">Setting and Configuraton</a></p>
+<p className="p-2 paragraph"><a href="">Intergrations</a></p>
+<p className="p-2 paragraph"><a href="">Security</a></p>
+<p className="p-2 text-[16px] paragraph"><a href="">Other Features</a></p>
+<Button className="items-center bg-sky-800 p-1 rounded-lg text-center text-white">Logout</Button>
+  </div>
+)}
+
+        </div>
             <img src={logo} alt="logo" className="object-cover"/>
-            <div className="flex justify-center items-center bg-slate-200 hover:bg-white shadow-md mt-7 rounded w-[30px] h-[30px] r-0">  
+            <div className="flex justify-center items-center bg-slate-200 hover:bg-white shadow-md mr-1 border border-black rounded w-[35px] h-[35px] r-0">  
     <MdLightMode onClick={()=>handlethemechange()}  />
 
     </div>
         </div>
-        <div className="fixed flex justify-center items-center bg-slate-200 opacity-80 shadow shadow-black backdrop-blur-md mt-24 rounded-md w-[35px] h-[35px]"> 
-            <CiMenuKebab  onClick={()=>setLinks(!Links)} className="p-1 w-full h-full font-bold"/>
-                {Links && (
-                <div className="relative justify-center items-center bg-white w-[400px] menu"> <ul>
-                    <li><a href="" >Add Product</a></li>
-                    <li><a href="" >Manage Product</a></li>
-                    <li><a href="" >Promotions</a></li>
-                    <li><a href="" >Add Product</a></li>
-                    <li><a href="" >Add Product</a></li>
-                    </ul></div>
-                )}
-            </div>
+       
      </div>
    )
  }
